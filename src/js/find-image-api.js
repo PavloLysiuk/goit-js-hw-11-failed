261b8b9d9ed5518beacfbd57f71cf86cf4f08312
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-07-23 19:57:36 +0300

    update api js

--- src/js/find-image-api.js
+++ src/js/find-image-api.js
@@ -1,4 +1,16 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/'
-const API_KEY = '38328018-adf92d25e5f0a3816743083dd';
+export async function getGallery() {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '38328018-adf92d25e5f0a3816743083dd';
+  axios.defaults.baseURL = BASE_URL;
+
+  const options = {
+    url: BASE_URL,
+    method: 'GET',
+    params: {
+      key: API_KEY,
+      q: `${this.searchQuery}`,
+    },
+  };
+}
