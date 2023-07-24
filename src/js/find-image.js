7125c471ac7693c40224f75250079ee88cc18bc4
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-07-24 16:20:30 +0300

    update

--- src/js/find-image.js
+++ src/js/find-image.js
@@ -1,59 +1,80 @@
 import { getGallery } from './find-image-api';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import createMarkup from './create-markup';
+import { lightbox } from './simple-lightbox';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const selectors = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  loadMoreGuard: document.querySelector('.js-guard'),
+};
 
+const simpleLightbox = new SimpleLightbox('.photo-card a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
 
+selectors.form.addEventListener('submit', onSearchClick);
 
+const options = {
+  root: null,
+  rootMargin: '400px',
+  threshold: 0,
+};
 
+const observer = new IntersectionObserver(onInfiniteScroll, options);
 
+let searchQuery = '';
+let page;
 
+async function onSearchClick(e) {
+  e.preventDefault();
+  selectors.gallery.innerHTML = '';
+  page = 1;
+  searchQuery = e.currentTarget.searchQuery.value;
+  if (!searchQuery.trim()) {
+    return;
+  }
+  await renderCards();
+  if (selectors.gallery.children.length === 40) {
+    observer.observe(selectors.loadMoreGuard);
+  }
+}
 
-
-
-
-
-function createMarkup(data) {
-  return data.map(
-    ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      return `<div class="image-item">
-
-    <a href="${largeImageURL}">
-
-      <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
-    </a>
-
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-        ${likes}
-      </p>
-
-      <p class="info-item">
-        <b>Views</b>
-        ${views}
-      </p>
-
-      <p class="info-item">
-        <b>Comments</b>
-        ${comments}
-      </p>
-
-      <p class="info-item">
-        <b>Downloads</b>
-        ${downloads}
-      </p>
-    </div>
-    </div>`;
+function onInfiniteScroll(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      onLoadeMore();
     }
-  );
+  });
+}
+
+async function onLoadeMore() {
+  page += 1;
+  await renderCards();
+}
+
+async function renderCards() {
+  try {
+    const {
+      data: { hits, totalHits },
+    } = await getGallery(searchQuery, page);
+    if (!totalHits) {
+      throw new Error('Empty');
+    } else if (page === 1) {
+      Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+    }
+    selectors.gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+    if (totalHits <= page * 40) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      observer.unobserve(selectors.loadMoreGuard);
+    }
+  } catch (error) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  simpleLightbox.refresh();
 }
