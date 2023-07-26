ee45803da813b0be98afa6846fa631f6f231b5d9
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-07-26 17:02:30 +0300

    Update find-image-api.js

--- src/js/find-image-api.js
+++ src/js/find-image-api.js
@@ -6,41 +6,45 @@ const API_KEY = '38328018-adf92d25e5f0a3816743083dd';
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-const params = {
-  key: API_KEY,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  per_page: 40,
-  safesearch: true,
-};
-
 export default class AxiosApiService {
   constructor() {
-    this.searchQuery = '';
-    this.page = 1;
+    this.axiosConfig = {
+      method: 'GET',
+      baseURL: BASE_URL,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: {
+        key: API_KEY,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        per_page: 40,
+        safesearch: true,
+        searchQuery: '',
+        page: 1,
+      },
+    };
   }
 
   async fetchImages() {
-    const url = `?q=${this.searchQuery}&page=${this.page}`;
-    // const response = await axios(url);
-    const response = await axios.get(url, { params: params });
+    const response = await axios(this.axiosConfig);
     this.incrementPage();
     return response.data;
   }
 
   incrementPage() {
-    this.page += 1;
+    this.axiosConfig.params.page += 1;
   }
 
   resetPage() {
-    this.page = 1;
+    this.axiosConfig.params.page = 1;
   }
 
   get query() {
-    return this.searchQuery;
+    return this.axiosConfig.params.searchQuery;
   }
 
   set query(newQuery) {
-    this.searchQuery = newQuery;
+    this.axiosConfig.params.searchQuery = newQuery;
   }
 }
