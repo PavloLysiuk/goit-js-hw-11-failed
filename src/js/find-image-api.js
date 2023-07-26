9070e4c64dea9c66d4dd63348b9fef4dc55b5c01
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-07-26 17:25:56 +0300

    Update find-image-api.js

--- src/js/find-image-api.js
+++ src/js/find-image-api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38328018-adf92d25e5f0a3816743083dd';
 
+
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
